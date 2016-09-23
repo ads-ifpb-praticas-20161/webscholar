9abd30c58d33dd9fa77fe0d3c6346d9272849c97
Marcus Vinícius <vmvini@hotmail.com>
2016-09-23 02:16:48 -0300

    course register jsf page

---
+++ webscholar-web/src/main/java/jsfbeans/CoursesMB.java
@@ -0,0 +1,53 @@
+package jsfbeans;
+
+import dac.webscholar.sessionbeans.course.CourseProxyQualifier;
+import dac.webscholar.sessionbeans.course.CourseServiceLocal;
+import dac.webscholar.shared.entities.Course;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+/**
+ * Created by marcusviniv on 23/09/2016.
+ */
+
+@Named
+@RequestScoped
+public class CoursesMB {
+
+    private Course newCourse;
+
+    @Inject
+    @CourseProxyQualifier
+    private CourseServiceLocal courseService;
+
+    @Inject
+    private FacesMessagesFacade fmf;
+
+    @PostConstruct
+    private void init(){
+        newCourse = new Course();
+    }
+
+    public void salvar(){
+        try {
+            courseService.saveCourse(newCourse);
+            fmf.successMsg("sucesso ao cadastrar " + newCourse.getName(), null);
+        }
+        catch(RuntimeException e){
+            fmf.errorMsg(e.getMessage(), null);
+        }
+
+    }
+
+    public Course getNewCourse(){
+        return newCourse;
+    }
+
+    public void setNewCourse(Course newCourse) {
+        this.newCourse = newCourse;
+    }
+
+
+}
---
+++ webscholar-web/src/main/java/jsfbeans/RoomsMB.java
@@ -0,0 +1,13 @@
+package jsfbeans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/**
+ * Created by marcusviniv on 23/09/2016.
+ */
+
+@Named
+@RequestScoped
+public class RoomsMB {
+}
